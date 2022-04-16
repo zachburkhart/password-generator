db6016aa051752ce842cb813fd7a3f6fc1b306f2
Zach Burkhart <zachburkhart216@gmail.com>
2022-04-16 14:13:48 -0400

    adding some variables for window prompts

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,28 @@
 // Assignment code here
-
-
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
+
+var selectCriteria = window.alert("Select password criteria");
+var selectLowercase = window.prompt("Include lowercase letters?");
+var selectUppercase = window.prompt("Include uppercase letters?");
+var selectNumeric = window.prompt("Include numbers?");
+var selectSpecial = window.prompt("Include special characters?");
+
+// character types
+// lowercase, uppercase, numeric, special characters
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 // Write password to the #password input
 function writePassword() {
@@ -15,4 +35,3 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-writepassword();
