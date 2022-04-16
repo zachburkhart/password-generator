9f6df6adf3b6ad2876b94d0e617ec1db252b2059
Zach Burkhart <zachburkhart216@gmail.com>
2022-04-16 16:27:52 -0400

    removed criteria function and added questions to generatePassword function because they variables weren't global so they couldn't be accessed by the generatePasssword function

--- assets/script.js
+++ assets/script.js
@@ -2,16 +2,18 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+// character types
+// lowercase, uppercase, numeric, special characters
 // Setting up an array for all the possible letters, numbers, and special characters
 var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var special = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")"];
 
-// character types
-// lowercase, uppercase, numeric, special characters
-// use this function to ask initial criteria questions
-function selectCriteria() {
+
+// this is the generatePassword function that's called when you click the button
+function generatePassword() {
+  // ask criteria questions
   window.alert("Select password criteria");
   var selectLength = window.prompt("How long do you want your password to be? Must be at least 8 and no more than 128.");
   var selectLowercase = window.prompt("Include lowercase letters? Enter YES or NO");
@@ -23,42 +25,29 @@ function selectCriteria() {
     + "Lowercase: " + selectLowercase + " -- " 
     + "Uppercase: " + selectUppercase + " -- " 
     + "Numbers: " + selectNumeric + " -- "
-    + "Special: " + selectSpecial)
-};
-
-// this is the generatePassword function that's called when you click the button
-function generatePassword() {
-  // ask criteria questions
-  var criteria = selectCriteria();
+    + "Special: " + selectSpecial);
+  var combinationBuild = [];
+  var completedPassword = "";
   //gather the responses to generate a password
-  if (selectLength > 7 && selectLength < 129) {
-    
-  }
   if (selectLowercase === "yes" || selectLowercase === "YES") {
-
+    for (var i of lower)
+      combinationBuild.push(i);
   };
   if (selectUppercase === "yes" || selectUppercase === "YES") {
-  
+    for (var i of upper)
+    combinationBuild.push(i);
   };
   if (selectNumeric === "yes" || selectNumeric === "YES") {
-
+    for (var i of number)
+      combinationBuild.push(i);
   };
   if (selectSpecial === "yes" || selectSpecial === "YES") {
+    for (var i of special)
+      combinationBuild.push(i);
+  }
 
-  };
-
-
-
-
-
-
-
-
-
-
-
-
-
+console.log(combinationBuild);
+}
 
 
 // Write password to the #password input
