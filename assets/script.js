b1792ddd4e113c70529ac97985dbff6745ca561d
Zach Burkhart <zachburkhart216@gmail.com>
2022-04-16 15:21:44 -0400

    the stylesheet and js weren't linked correctly lol. fixed it

--- assets/script.js
+++ assets/script.js
@@ -2,14 +2,26 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-var selectCriteria = window.alert("Select password criteria");
-var selectLowercase = window.prompt("Include lowercase letters?");
-var selectUppercase = window.prompt("Include uppercase letters?");
-var selectNumeric = window.prompt("Include numbers?");
-var selectSpecial = window.prompt("Include special characters?");
-
 // character types
 // lowercase, uppercase, numeric, special characters
+function selectCriteria() {
+  window.alert("Select password criteria");
+  var selectLowercase = window.confirm("Include lowercase letters?");
+  var selectUppercase = window.confirm("Include uppercase letters?");
+  var selectNumeric = window.confirm("Include numbers?");
+  var selectSpecial = window.confirm("Include special characters?");
+}
+
+// this is the generatePassword function that's called when you click the button
+// use this function to ask initial criteria questions
+function generatePassword() {
+  var criteria = selectCriteria();
+}
+
+// length of password
+var selectLength = window.prompt("How long do you want your password to be?");
+// use this function to meet acceptance criteria: must be between 8 and 128 characters
+
 
 
 
